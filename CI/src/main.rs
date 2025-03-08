dcbdbe49ee60f92ca685edef8b25ffdd4084ef5f
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-03-08 09:34:38 +0900

    Update main.rs

--- CI/src/main.rs
+++ CI/src/main.rs
@@ -1,3 +1,4 @@
+#[allow(unused)]
 fn execute_command(command: &[&str]) -> std::process::Output {
     std::process::Command::new(command[0])
         .args(&command[1..])
@@ -5,10 +6,77 @@ fn execute_command(command: &[&str]) -> std::process::Output {
         .expect("failed to execute process")
 }
 
+/// ファイルの MD5 をダンプ
+fn dump_file_md5(path: &str) -> std::result::Result<String, Box<dyn std::error::Error>> {
+    use std::io::Read;
+
+    // ファイルを開きます。
+    let mut file = std::fs::File::open(path)?;
+
+    // 読み取り用バッファ
+    let mut buffer = std::vec::Vec::new();
+
+    // ファイル全体を読み込み
+    file.read_to_end(&mut buffer)?;
+
+    // MD5 を計算
+    let response = md5::compute(buffer);
+
+    return Ok(format!("{:x}", response));
+}
+
+/// ファイル診断
+#[allow(unused)]
+fn diagnose_files(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let directory = std::fs::read_dir(path)?;
+    for entry in directory {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_dir() {
+            continue;
+        }
+        let path_info = std::fs::metadata(&path)?;
+        let length = path_info.len();
+        let written_at = path_info.modified()?;
+        let md5sum = dump_file_md5(&path.to_string_lossy())?;
+        println!("{}, {}, {}", path.to_string_lossy(), length, md5sum);
+    }
+
+    return Ok(());
+}
+
+/// OS を考慮したパス結合
+fn concat_path_parts(parts: &[&str]) -> String {
+    return parts.join(&std::path::MAIN_SEPARATOR.to_string());
+}
+
+/// 実行
+fn execute() -> Result<(), Box<dyn std::error::Error>> {
+    chdir("..")?;
+
+    // 開始位置のパス
+    let paths = concat_path_parts(&["src", "components", "chakra"]);
+
+    // 診断
+    let _files = diagnose_files(&paths)?;
+
+    return Ok(());
+}
+
+fn chdir(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    std::env::set_current_dir(path)?;
+    return Ok(());
+}
+
 fn main() {
     println!("### START ###");
 
-    execute_command(&["find", "src/components/chakra", "-type", "f"]);
+    let result = execute();
+    if result.is_err() {
+        println!("Error: {:?}", result.err());
+        std::process::exit(1);
+    }
 
+    // let result = execute_command(&["find", "src/components/chakra", "-type", "f"]);
     println!("--- END ---");
 }
